24120ca1ba6cac9910639502a0da3e576fb15bf6
Aailmz <mirzazubaridjunaid@gmail.com>
2024-08-13 18:07:24 +0700

    First

---
+++ CashierScan/Cashier.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace CashierScan
+{
+    partial class Cashier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.qrBox = new System.Windows.Forms.PictureBox();
+            this.textCode = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.gridGoods = new System.Windows.Forms.DataGridView();
+            this.gridScan = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.qrBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridGoods)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridScan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Name);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.qrBox);
+            this.panel1.Controls.Add(this.textCode);
+            this.panel1.Controls.Add(this.textName);
+            this.panel1.Controls.Add(this.gridGoods);
+            this.panel1.Controls.Add(this.gridScan);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 425);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Code";
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(3, 5);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(42, 13);
+            this.Name.TabIndex = 8;
+            this.Name.Text = "Cashier";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(4, 223);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(185, 34);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Gold;
+            this.btnEdit.Location = new System.Drawing.Point(4, 167);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(185, 34);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnAdd.Location = new System.Drawing.Point(4, 105);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(185, 34);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // qrBox
+            // 
+            this.qrBox.Location = new System.Drawing.Point(195, 105);
+            this.qrBox.Name = "qrBox";
+            this.qrBox.Size = new System.Drawing.Size(151, 152);
+            this.qrBox.TabIndex = 4;
+            this.qrBox.TabStop = false;
+            // 
+            // textCode
+            // 
+            this.textCode.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textCode.Location = new System.Drawing.Point(-1, 66);
+            this.textCode.Name = "textCode";
+            this.textCode.Size = new System.Drawing.Size(346, 20);
+            this.textCode.TabIndex = 3;
+            // 
+            // textName
+            // 
+            this.textName.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textName.Location = new System.Drawing.Point(0, 21);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(346, 20);
+            this.textName.TabIndex = 2;
+            // 
+            // gridGoods
+            // 
+            this.gridGoods.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.gridGoods.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridGoods.Location = new System.Drawing.Point(0, 263);
+            this.gridGoods.Name = "gridGoods";
+            this.gridGoods.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridGoods.Size = new System.Drawing.Size(346, 162);
+            this.gridGoods.TabIndex = 1;
+            this.gridGoods.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridGoods_CellClick);
+            // 
+            // gridScan
+            // 
+            this.gridScan.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.gridScan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridScan.Location = new System.Drawing.Point(362, -1);
+            this.gridScan.Name = "gridScan";
+            this.gridScan.Size = new System.Drawing.Size(413, 426);
+            this.gridScan.TabIndex = 0;
+            // 
+            // Cashier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name.Text = "Cashier";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cashier";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.qrBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridGoods)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridScan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView gridGoods;
+        private System.Windows.Forms.DataGridView gridScan;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.PictureBox qrBox;
+        private System.Windows.Forms.TextBox textCode;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Name;
+    }
+}
---
+++ CashierScan/Cashier.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using QRCoder;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace CashierScan
+{
+    public partial class Cashier : Form
+    {
+        string connectionString = "server=localhost;database=cashier_scan;uid=root;pwd=;";
+        private HttpListener listener;
+        private Thread listenerThread;
+
+        public Cashier()
+        {
+            InitializeComponent();
+            LoadData();
+            StartHttpServer();
+
+            gridScan.Columns.Add("Name", "Name");
+            gridScan.Columns.Add("Code", "Code");
+
+            gridScan.Columns.Add("Timestamp", "Timestamp"); // Contoh kolom baru
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM goods";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    gridGoods.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void GenerateQRCode(string name, string code)
+        {
+            var item = new
+            {
+                Name = name,
+                Code = code,
+            };
+
+            string qrText = JsonConvert.SerializeObject(item);
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(10);
+
+            qrBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            qrBox.Image = qrCodeImage;
+        }
+
+        public class ScannedItem
+        {
+            public string Name { get; set; }
+            public string Code { get; set; }
+        }
+
+        private void StartHttpServer()
+        {
+            try
+            {
+                listener = new HttpListener();
+                listener.Prefixes.Add("http://192.168.1.150:8000/");
+                listener.Start();
+                Console.WriteLine("Server started, listening on http://192.168.1.150:8000/");
+                listenerThread = new Thread(Listen);
+                listenerThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error starting HTTP server: " + ex.Message);
+            }
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                while (listener.IsListening)
+                {
+                    var context = listener.GetContext();
+                    var request = context.Request;
+                    var response = context.Response;
+
+                    // Add CORS headers
+                    response.Headers.Add("Access-Control-Allow-Origin", "*");
+                    response.Headers.Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+                    response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+
+                    if (request.HttpMethod == "OPTIONS")
+                    {
+                        response.StatusCode = 204;
+                        response.Close();
+                        continue;
+                    }
+
+                    if (request.HttpMethod == "POST" && request.HasEntityBody)
+                    {
+                        using (var body = request.InputStream)
+                        using (var reader = new StreamReader(body, request.ContentEncoding))
+                        {
+                            var json = reader.ReadToEnd();
+                            var item = JsonConvert.DeserializeObject<ScannedItem>(json);
+
+                            // Tambahkan timestamp
+                            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                            Invoke(new Action(() =>
+                            {
+                                gridScan.Rows.Add(item.Name, item.Code, timestamp); // Tambahkan timestamp
+                            }));
+                        }
+                    }
+
+                    response.StatusCode = 200;
+                    response.Close();
+                }
+            }
+            catch (HttpListenerException hle)
+            {
+                Console.WriteLine("HttpListenerException: " + hle.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+
+
+        private void Cashier_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (listener != null && listener.IsListening)
+            {
+                listener.Stop();
+            }
+
+            if (listenerThread != null && listenerThread.IsAlive)
+            {
+                listenerThread.Abort();
+            }
+        }
+
+        // Implementasi event handler untuk tombol-tombol lainnya...
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textName.Text;
+                string code = textCode.Text;
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO goods (name, code) VALUES (@name, @code)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@name", name);
+                        cmd.Parameters.AddWithValue("@code", code);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Data successfully inserted!");
+                            LoadData();
+
+                            // Generate QR Code after successful data insertion
+                            GenerateQRCode(name, code);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Data insertion failed.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (gridGoods.SelectedRows.Count > 0)
+                {
+                    string selectedCode = gridGoods.SelectedRows[0].Cells["code"].Value.ToString();
+                    string name = textName.Text;
+                    string code = textCode.Text;
+
+                    using (MySqlConnection conn = new MySqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string query = "UPDATE goods SET name = @name, code = @code WHERE code = @selectedCode";
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@name", name);
+                            cmd.Parameters.AddWithValue("@code", code);
+                            cmd.Parameters.AddWithValue("@selectedCode", selectedCode);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Data successfully updated!");
+                                LoadData();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Data update failed.");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a record to update.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (gridGoods.SelectedRows.Count > 0)
+                {
+                    string selectedCode = gridGoods.SelectedRows[0].Cells["code"].Value.ToString();
+
+                    using (MySqlConnection conn = new MySqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string query = "DELETE FROM goods WHERE code = @selectedCode";
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@selectedCode", selectedCode);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Data successfully deleted!");
+                                LoadData();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Data deletion failed.");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a record to delete.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void gridGoods_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = gridGoods.Rows[e.RowIndex];
+
+                string name = row.Cells["name"].Value?.ToString();
+                string code = row.Cells["code"].Value?.ToString();
+
+                textName.Text = name;
+                textCode.Text = code;
+
+                GenerateQRCode(name, code);
+            }
+        }
+    }
+}
